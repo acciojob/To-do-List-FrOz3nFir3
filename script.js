cd45d19d42423ab01d9507ba117a489bb9058c6b
Kush Surti <kushsurti05@gmail.com>
2022-12-20 17:20:40 +0530

    basic todo list functionality added

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-//your code here
+const input = document.getElementById("newTodoInput");
+const addButton = document.getElementById("addTodoBtn");
+const olList = document.getElementById("todoList");
 
+addButton.addEventListener("click", addItemToList);
+function addItemToList() {
+  let value = input.value.trim();
+  if (value.length == 0) return;
+
+  let li = document.createElement("li");
+  li.innerText = value;
+  olList.append(li);
+  // reset input
+  input.value = "";
+}
